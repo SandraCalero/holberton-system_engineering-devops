ba53bde3157888def3870fffdcbadf45f34cc7b2
Sandra Calero <3358@holbertonschool.com>
2022-01-04 21:31:41 -0500

    add a puppet manifest that makes changes to the configuration file

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,9 +1,11 @@
 # Puppet manifest that makes changes to the configuration file
 include stdlib
-file_line { '/etc/ssh/ssh_config':
-  line =>  'PasswordAuthentication no',
+file_line { 'Set PasswordAuthentication':
+    path => '/etc/ssh/ssh_config',
+    line => 'PasswordAuthentication no',
 }
 
-file_line {  '/etc/ssh/ssh_config':
-  line =>  'IdentityFile ~/.ssh/school',
+file_line { 'Add IdentityFile to use the private key ~/.ssh/school':
+    path => '/etc/ssh/ssh_config',
+    line => 'IdentityFile ~/.ssh/school',
 }
