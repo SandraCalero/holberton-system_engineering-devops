3f6d07a32d9f0b3e93c64c3a18afb16e55f24b6f
Sandra Calero <3358@holbertonschool.com>
2022-01-06 22:48:01 -0500

    add a puppet manifests that configures Nginx server

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,7 +1,4 @@
 # Puppet manifests that configures Nginx server
-include nginx
-
-nginx::resource::server { 'ubuntu.35.196.20.175':
-  listen_port => 80,
-  proxy       => 'http://localhost:5601',
+package { 'nginx':
+    ensure => installed,
 }
