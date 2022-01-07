3dd5fb0b96a0886d901816811f9a79d57e17166c
Sandra Calero <3358@holbertonschool.com>
2022-01-06 22:35:01 -0500

    add a puppet manifests that configures Nginx server

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,30 +1,7 @@
 # Puppet manifests that configures Nginx server
-exec { 'sudo apt-get -y update':
-    path    => '/usr/bin/',
-}
+include nginx
 
-exec { 'sudo apt-get -y install nginx':
-    path    => '/usr/bin/',
-}
-
-exec { 'sudo ufw allow 'Nginx HTTP'':
-    path    => '/usr/bin/',
-}
-
-exec { 'echo "Hello World" > /var/www/html/index.html':
-    path    => '/usr/bin/',
-}
-
-exec { 'new_page="https://www.youtube.com/watch?v=QH2-TGUlwu4"':
-    path    => '/usr/bin/',
-}
-exec { 'file="/etc/nginx/sites-available/default"':
-    path    => '/usr/bin/',
-}
-exec { 'sudo sed -i "/listen 80 default_server/a rewrite ^/redirect_me $new_page permanent;" $file':
-    path    => '/usr/bin/',
-}
-
-exec { 'sudo service nginx restart':
-    path    => '/usr/bin/',
+nginx::resource::server { 'ubuntu.35.196.20.175':
+  listen_port => 80,
+  proxy       => 'http://localhost:5601',
 }
