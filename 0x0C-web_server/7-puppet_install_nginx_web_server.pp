0e4b2fc58033160bbe228b695842d5bf0f965c52
Sandra Calero <3358@holbertonschool.com>
2022-01-06 23:04:45 -0500

    add a puppet manifests that configures Nginx server

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,8 +1,17 @@
 # Puppet manifests that configures Nginx server
+
+#Install nginx
 package { 'nginx':
     ensure => installed,
 }
 
+#Create a page that contains the string Hellow World
+file { '/var/www/html/index.html':
+    ensure  => file,
+    content => 'Hellow World',
+}
+
+# Equivalent to the command service nginx restart
 service { 'nginx':
   ensure => running,
   require => Package['nginx'],
