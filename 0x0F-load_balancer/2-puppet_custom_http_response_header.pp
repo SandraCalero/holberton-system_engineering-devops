dfc7ff77a5128e24bc6403977f884753f410f155
Sandra Calero <3358@holbertonschool.com>
2022-01-10 17:41:27 -0500

    add a puppet manifest that configures Nginx server

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,31 @@
+# Puppet manifests that configures Nginx server
+
+#Install nginx
+package { 'nginx':
+    ensure => installed,
+}
+
+#Create a page that contains the string Hellow World
+file { '/var/www/html/index.html':
+    content =>  'Hello World ',
+}
+
+# Configure Nginx server so that /redirect_me is redirecting to another page.
+file_line { 'redirect 301 Moved Permanently':
+    path  => '/etc/nginx/sites-available/default',
+    after => 'listen 80 default_server',
+    line  => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+}
+
+# Create a custom HTTP header response.
+file_line { 'header':
+    path  => '/etc/nginx/sites-available/default',
+    after => 'listen 80 default_server',
+    line  => 'add_header X-Served-By $HOSTNAME;',
+}
+
+# Equivalent to the command service nginx restart
+service { 'nginx':
+    ensure  => running,
+    require => Package['nginx'],
+}
