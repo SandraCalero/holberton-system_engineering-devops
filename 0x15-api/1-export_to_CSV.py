89f4fe85ce147ab10b8caf8fde02d19a762b15c4
Sandra Calero <3358@holbertonschool.com>
2022-01-31 23:39:33 -0500

    add a script that Records all tasks that are owned by an employee

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -2,27 +2,31 @@
 """Records all tasks that are owned by an employee
 """
 import csv
+import json
 import requests
-from sys import argv
+import sys
 
-if __name__ == "__main__":
-    employee_ID = int(argv[1])
 
-    url = 'https://jsonplaceholder.typicode.com/'
-
-    user = requests.get(url + 'users/' + str(employee_ID)).json()
-    username = user['username']
-
-    todo = requests.get(url + 'todos').json()
-    todo_user = [task for task in todo if task['userId'] == int(employee_ID)]
-
+if __name__ == '__main__':
+    employee_ID = int(sys.argv[1])
+    todos_data = requests.get('https://jsonplaceholder.typicode.com/todos')
+    todos = json.loads(todos_data.text)
+    employee_data = requests.get('https://jsonplaceholder.typicode.com/users')
+    employees = json.loads(employee_data.text)
+    '''Creating row'''
+    row = []
+    '''Getting employee username'''
+    for empl in employees:
+        if empl['id'] == employee_ID:
+            employee_username = empl['username']
+    '''Write to csv file'''
     with open('{}.csv'.format(employee_ID), 'w', encoding='UTF8') as f:
         writer = csv.writer(f, delimiter=',', quotechar='"',
                             quoting=csv.QUOTE_ALL)
-        for task in todo_user:
-            if task['userId'] == employee_ID:
+        for todo in todos:
+            if todo['userId'] == employee_ID:
                 row.append(employee_ID)
-                row.append(username)
+                row.append(employee_username)
                 row.append(todo['completed'])
                 row.append(todo['title'])
                 writer.writerow(row)
