2013796e1b7ce35ac68ee3b8c3593caa72bd5ed8
Sandra Calero <3358@holbertonschool.com>
2022-01-31 23:23:48 -0500

    add a script that Records all tasks that are owned by an employee

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
     todo = requests.get(url + 'todos').json()
     todo_user = [task for task in todo if task['userId'] == int(employee_ID)]
 
-    with open(str(employee_ID) + '.csv', 'w') as filename:
+    with open('{}.csv'.format(employee_ID), 'w') as filename:
         writer = csv.writer(filename, quoting=csv.QUOTE_ALL)
         for task in todo_user:
             data = [employee_ID, username, task['completed'], task['title']]
